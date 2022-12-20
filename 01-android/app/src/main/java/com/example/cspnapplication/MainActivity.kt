8c69d25648edb3d11f39c49bc6df2c7b84394a0e
Cristopher Pérez <cristopher.perez@epn.edu.ec>
2022-12-20 10:49:55 -0500

    Botón y funcionalidades

--- 01-android/app/src/main/java/com/example/cspnapplication/MainActivity.kt
+++ 01-android/app/src/main/java/com/example/cspnapplication/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.cspnapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // Se define qué interfaz vamos a utilizar.
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida
+            .setOnClickListener {
+                irActividad(ACicloVida::class.java)
+        }
+    }
+
+
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
