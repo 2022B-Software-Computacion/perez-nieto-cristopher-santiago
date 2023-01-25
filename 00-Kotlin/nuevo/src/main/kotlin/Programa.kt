10a396cd5bedec354c88bf3e9668eaa76b4cd49e
Cristopher Pérez <cristopher.perez@epn.edu.ec>
2023-01-25 12:46:23 -0500

    Recycler View Implementado

--- 00-Kotlin/nuevo/src/main/kotlin/Programa.kt
+++ 00-Kotlin/nuevo/src/main/kotlin/Programa.kt
@@ -8,3 +8,25 @@ fun main(){
     println("4")
 
 }
+
+abstract class NumerosJava1{
+    protected val numeroUno: Int;
+    protected val numeroDos: Int;
+
+    constructor(
+        uno: Int,
+        dos: Int
+    ){//Bloque codigo constructor
+        this.numeroUno = uno;
+        this.numeroDos = dos;
+        println("Inicializando");
+    }
+
+    /*init { // Bloque de código del constructor PRIMARIO
+        this.numeroUno
+        numeroUno
+        this.numeroDos
+        numeroDos
+        println("Inicializado");
+    }*/
+}
